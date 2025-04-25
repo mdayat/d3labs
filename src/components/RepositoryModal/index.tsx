f7d5f5421306e6bad0c8d25410272911bdef7119
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2025-04-26 00:47:42 +0700

    add error handling when displaying README as modal

--- src/components/RepositoryModal/index.tsx
+++ src/components/RepositoryModal/index.tsx
@@ -3,6 +3,8 @@ import DOMPurify from "dompurify";
 import { marked } from "marked";
 import { axiosInstance } from "@libs/axios";
 import styles from "./RepositoryModal.module.css";
+import { useSelectedUser } from "@contexts/SelectedUserProvider";
+import { toast } from "react-toastify";
 
 interface RepositoryModalProps {
   repositoryName: string;
@@ -12,34 +14,42 @@ interface RepositoryModalProps {
 function RepositoryModal({ repositoryName, setIsOpen }: RepositoryModalProps) {
   const [readmeContent, setReadmeContent] = useState("");
 
+  const { selectedUser } = useSelectedUser();
+
   useEffect(() => {
     (async () => {
-      const res = await axiosInstance.get<{ content: string }>(
-        `https://api.github.com/repos/${"mdayat"}/${repositoryName}/readme`,
-        {
+      try {
+        const url = `https://api.github.com/repos/${
+          selectedUser!.login
+        }/${repositoryName}/readme`;
+
+        const res = await axiosInstance.get<{ content: string }>(url, {
           headers: {
             Accept: "application/vnd.github+json",
             Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`,
             "X-GitHub-Api-Version": "2022-11-28",
           },
+        });
+
+        if (!res.data.content) {
+          setReadmeContent(
+            `<p style="text-align: center; font-style: italic;">This repository doesn't have README</p>`
+          );
+          return;
         }
-      );
 
-      if (!res.data.content) {
-        setReadmeContent("<p>This repository doesn't have README</p>");
-        return;
-      }
-
-      try {
         const decodedContent = atob(res.data.content);
         const html = DOMPurify.sanitize(await marked.parse(decodedContent));
         setReadmeContent(html);
       } catch (error) {
-        console.error("error processing README:", error);
-        setReadmeContent("<p>Error processing README content</p>");
+        console.error("failed to get README", { cause: error });
+        toast("Something is wrong when displaying README, please try again", {
+          type: "error",
+          theme: "colored",
+        });
       }
     })();
-  }, [repositoryName]);
+  }, [repositoryName, selectedUser]);
 
   // Prevent body scrolling when modal is open
   useEffect(() => {
